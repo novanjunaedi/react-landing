8cc7b7d5761955326cc7241cca3721b5ec835eef
Novan Junaedi <junaedinovan98@gmail.com>
2025-01-14 11:13:07 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "@/components/global/Footer";
+import Navbar from "@/components/global/Navbar";
+import Hero from "@/components/home/Hero";
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
